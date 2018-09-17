c4a31349020a61d0e6d29cbceffbf7abbcef759a
Junseo <lowapple99@gmail.com>
2018-09-17 23:55:48 +0900

    Update Slack bot

--- CorpListParser.py
+++ CorpListParser.py
@@ -1,6 +1,8 @@
 import asyncio
 import aiohttp
 from bs4 import BeautifulSoup
+from slacker import Slacker
+from config import Config
 
 async def downloadCorpList():
     async with aiohttp.ClientSession() as session:
@@ -14,8 +16,20 @@ async def downloadCorpList():
                 corp_code = corp_detail[0].find('a').text
                 corp_name = corp_detail[1].text
                 corp_type = corp_detail[2].text
-                
+
                 print(corp_code + corp_name + corp_type)
+
+            slack_body = dict()
+            slack_body['title'] = "종목코드가 전부 업데이트 되었습니다."
+            slack_body['fallback'] = "종목코드가 전부 업데이트 되었습니다."
+
+            attachments = [slack_body]
+
+            slack = Slacker(Config().slack_token)
+            slack.chat.post_message(
+                channel='#corp_update', 
+                text=None,
+                attachments=attachments)
 
 
 loop = asyncio.get_event_loop()
